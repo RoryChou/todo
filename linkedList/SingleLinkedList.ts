a165d0d5bf3db87194ade978dcd8f497c02b5e6e
rory <365538079@qq.com>
2021-01-06 19:59:36 +0800

    嗷嗷嗷

---
+++ linkedList/SingleLinkedList.ts
@@ -0,0 +1,23 @@
+class SingleLinkedList {
+    val = ''
+    next = null
+    constructor() {
+        this.val = ''
+        this.next = null
+    }
+    get(index){
+
+    }
+    addAtHead(val){
+
+    }
+    addAtTail(val){
+
+    }
+    addAtIndex(index,val){
+
+    }
+    deleteAtIndex(index){
+
+    }
+}
---
+++ tree/tree.js
@@ -0,0 +1,13 @@
+// 二叉树的节点
+class Node {
+  constructor(val) {
+    this.val = val
+    this.left = this.right = undefined
+  }
+}
+
+class Tree {
+  constructor(data) {
+    // 临时存储所有节点，方便寻找父子节点
+  }
+}
